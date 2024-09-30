bdff0a190d7fc91ccadc4fdd5e6aec5805ab8be5
Felicialite19 <felicialite@gmail.com>
2024-09-30 19:27:05 +0400

    module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -2,7 +2,8 @@ def get_multiplied_digits(number):
     number = int(number)
     str_number = str(number)
     first = int(str_number[0])
-
+    while str_number.endswith('0'):  # убираем нули в конце number
+        str_number = str_number[:len(str_number) - 1]
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
